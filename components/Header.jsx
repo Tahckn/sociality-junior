c93f4ce249f33ae58a7cab9bb63b9deed5049e5e
Taha CEKEN <taha-ceken@hotmail.com>
2022-09-16 01:59:41 +0300

    reading data.json and rendering date data

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,39 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Header extends Component {
-  render() {
-    return (
-      <header>
-        <div className="status-container">
-          <div className="status-color">
-            <div id="status-color1"></div>
-            <p>Published</p>
-          </div>
-
-          <div className="status-color">
-            <div id="status-color2"></div>
-            <p>Scheduled</p>
-          </div>
-
-          <div className="status-color">
-            <div id="status-color3"></div>
-            <p>Need Approval</p>
-          </div>
-
-          <div className="status-color">
-            <div id="status-color4"></div>
-            <p>Error</p>
-          </div>
-
-          <div className="status-color">
-            <div id="status-color5"></div>
-            <p>Notes</p>
-          </div>
+const Header = () => {
+  return (
+    <header>
+      <div className="status-container">
+        <div className="status-color">
+          <div id="status-color1"></div>
+          <p>Published</p>
         </div>
-        <h1>14 January 2016</h1>
-      </header>
-    )
-  }
+
+        <div className="status-color">
+          <div id="status-color2"></div>
+          <p>Scheduled</p>
+        </div>
+
+        <div className="status-color">
+          <div id="status-color3"></div>
+          <p>Need Approval</p>
+        </div>
+
+        <div className="status-color">
+          <div id="status-color4"></div>
+          <p>Error</p>
+        </div>
+
+        <div className="status-color">
+          <div id="status-color5"></div>
+          <p>Notes</p>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import { BiBlock } from 'react-icons/bi'
 import { BiTrash } from 'react-icons/bi'
 import { TbChecks } from 'react-icons/tb'
@@ -9,52 +8,60 @@ import { BiComment } from 'react-icons/bi'
 import { BiShareAlt } from 'react-icons/bi'
 import { BiShow } from 'react-icons/bi'
 import Image from 'next/image'
+import myData from '../data.json'
 
-export class PostCard extends Component {
-  render() {
-    return (
-      <div className="post-container">
-        <div className="post-card">
-          <div className="post-card-social">
-            <ImFacebook size="23" color="white" />
+const postsByDate = JSON.stringify(myData)
+const parsedData = JSON.parse(postsByDate)
+
+const PostCard = () => {
+  console.log(postsByDate)
+  return (
+    <div className="post-container">
+      {Object.keys(myData.posts_by_date).map((key, i) => (
+        <div key={i}>
+          <h1>{key}</h1>
+        </div>
+      ))}
+      <div className="post-card">
+        <div className="post-card-social">
+          <ImFacebook size="23" color="white" />
+        </div>
+        <div className="post">
+          <div className="post-header">
+            <p className="date"></p>
+            <div className="actions">
+              <TbChecks size="20" color="#acacac" />
+              <BiBlock size="20" color="#acacac" />
+              <BiTrash size="20" color="#acacac" />
+              <HiOutlineDotsCircleHorizontal size="20" color="#acacac" />
+            </div>
           </div>
-          <div className="post">
-            <div className="post-header">
-              <p className="date">14 January 2016 - 14:30</p>
-              <div className="actions">
-                <TbChecks size="20" color="#acacac" />
-                <BiBlock size="20" color="#acacac" />
-                <BiTrash size="20" color="#acacac" />
-                <HiOutlineDotsCircleHorizontal size="20" color="#acacac" />
-              </div>
-            </div>
-            <p className="post-text">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae esse autem sunt tempore.
-            </p>
-            <div className="photo">
-              <Image
-                src="https://s3-eu-west-1.amazonaws.com/a6apptest/public/files/58bab4018803fa0008413733_0A0szw7OSDIVt2rq80.jpeg"
-                alt="tata"
-                width={283}
-                height={239}
-              />
-            </div>
-            <div className="reactions">
-              <BiLike size="20" color="#444444" />
-              <p>0</p>
-              <BiComment size="20" color="#444444" />
-              <p>0</p>
-              <BiShareAlt size="20" color="#444444" />
-              <p>0</p>
-              <BiShow size="21" color="#444444" />
-              <p>0</p>
-            </div>
+          <p className="post-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            esse autem sunt tempore.
+          </p>
+          <div className="photo">
+            <Image
+              src="https://s3-eu-west-1.amazonaws.com/a6apptest/public/files/58bab4018803fa0008413733_0A0szw7OSDIVt2rq80.jpeg"
+              alt="tata"
+              width={283}
+              height={239}
+            />
+          </div>
+          <div className="reactions">
+            <BiLike size="20" color="#444444" />
+            <p>0</p>
+            <BiComment size="20" color="#444444" />
+            <p>0</p>
+            <BiShareAlt size="20" color="#444444" />
+            <p>0</p>
+            <BiShow size="21" color="#444444" />
+            <p>0</p>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default PostCard
